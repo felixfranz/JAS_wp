89967108769dc22effb7154cfbcc06d73395b752
felixfranz <hello@felixfranz.com>
2025-04-14 11:22:42 +0200

    Overlay mobile +

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
  
 <!--OVERLAY NAV start -->    
 		<div class="nav_container_mobile">
-			
+			<div class="nav_content container">
 					<nav role="navigation" >
 
 						<?php wp_nav_menu(array(
@@ -46,6 +46,30 @@
 
           ?>
           </nav>
+          <nav>
+						<?php wp_nav_menu(array(
+    					         'container' => false,                           // remove nav container
+    					         'menu' => __( 'Secondary Menu', 'bonestheme' ),  // nav name
+    					         'menu_class' => 'nav secondary-overlay-nav',               // adding custom nav class
+    					         'theme_location' => 'secondary-nav',                 // where it's located in the theme
+    					         'before' => '',                                 // before the menu
+        			               'after' => '',                                  // after the menu
+        			               'link_before' => '',                            // before each link
+        			               'link_after' => '',                             // after each link
+        			               'depth' => 0,                                   // limit the depth of the nav
+    					         'fallback_cb' => ''                             // fallback function (if there is one)
+						)); ?>
+
+						</nav>
+
+            <div class="image-links">
+              
+              <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/logo_untis.jpg" /></a>
+              <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/logo_mensa.jpg" /></a>
+              <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/logo_iserv.jpg" /></a>
+
+            </div>
+        </div>
 		</div>
     <!-- OVERLAY NAV end -->  
 
